339f392159f8bc3aebeb41577fcd034100e7bebf
Thomas Antony <mail@thomasantony.com>
2025-12-29 15:35:54 -0800

    Add plane_broadcast example

--- src/main.rs
+++ src/main.rs
@@ -21,9 +21,11 @@ fn kernel_plane_broadcast(input_data: &Array<u32>, output_data: &mut Array<u32>)
     let thread_id = UNIT_POS;
 
     let local_data = input_data[block_id * CUBE_DIM + thread_id];
-    let local_sum = plane_exclusive_sum(local_data);
+    // Get the value of local_data from thread #7 (zero-indexed) in the plane/warp
+    let first_num_in_plane = plane_broadcast(local_data, 7);
 
-    output_data[block_id * CUBE_DIM + thread_id] = local_sum;
+    // Add it to the current value
+    output_data[block_id * CUBE_DIM + thread_id] = local_data + first_num_in_plane;
 }
 
 #[cube(launch)]
@@ -54,28 +56,11 @@ fn launch_double_numbers_kernel<R: Runtime>(
     }
 }
 
-fn launch_plane_exclusive_sum<R: Runtime>(
-    client: &ComputeClient<R::Server>,
-    input: &Handle,
-    output: &Handle,
-    num_elements: usize,
-    block_size: usize,
-) {
-    let num_blocks = num_elements / block_size;
-    unsafe {
-        kernel_plane_exclusive_sum::launch::<R>(
-            &client,
-            CubeCount::Static(num_blocks as u32, 1, 1),
-            CubeDim::new(block_size as u32, 1, 1),
-            ArrayArg::from_raw_parts::<u32>(input, num_elements, 1),
-            ArrayArg::from_raw_parts::<u32>(output, num_elements, 1),
-        )
-    }
-}
-
 fn main() {
     let device = Default::default();
     let client = cubecl::wgpu::WgpuRuntime::client(&device);
+
+    type R = cubecl::wgpu::WgpuRuntime;
 
     let input_data = (1..11).collect::<Vec<u32>>();
     println!("Input: {:?}", &input_data);
@@ -84,32 +69,48 @@ fn main() {
     let input_data_gpu = client.create(u32::as_bytes(&input_data));
     let output_data_gpu = client.create(u32::as_bytes(&zeros));
 
-    launch_double_numbers_kernel::<cubecl::wgpu::WgpuRuntime>(
-        &client,
-        &input_data_gpu,
-        3,
-        &output_data_gpu,
-        num_elements,
-    );
+    launch_double_numbers_kernel::<R>(&client, &input_data_gpu, 3, &output_data_gpu, num_elements);
     let result = client.read_one(output_data_gpu.clone());
     let output = u32::from_bytes(&result).to_vec();
     println!("Scale numbers kernel: {:?}\n", output);
 
     // Use larger data array for exclusive sum
-    let input_data = vec![1u32; 64];
+    let input_data = vec![1u32; 16];
     println!("Input: {:?}", &input_data);
     let num_elements = input_data.len();
     let zeros = vec![0u32; num_elements];
     let input_data_gpu = client.create(u32::as_bytes(&input_data));
     let output_data_gpu = client.create(u32::as_bytes(&zeros));
-    launch_plane_exclusive_sum::<cubecl::wgpu::WgpuRuntime>(
-        &client,
-        &input_data_gpu,
-        &output_data_gpu,
-        num_elements,
-        32,
-    );
+
+    const BLOCK_SIZE: usize = 8;
+    let num_blocks = num_elements / BLOCK_SIZE;
+    unsafe {
+        kernel_plane_exclusive_sum::launch::<R>(
+            &client,
+            CubeCount::Static(num_blocks as u32, 1, 1),
+            CubeDim::new(BLOCK_SIZE as u32, 1, 1),
+            ArrayArg::from_raw_parts::<u32>(&input_data_gpu, num_elements, 1),
+            ArrayArg::from_raw_parts::<u32>(&output_data_gpu, num_elements, 1),
+        )
+    }
     let result = client.read_one(output_data_gpu.clone());
     let output = u32::from_bytes(&result).to_vec();
     println!("Plane Exclusive Sum: {:?}", output);
+
+    let input_data = (0..16).collect::<Vec<u32>>();
+    println!("Input: {:?}", &input_data);
+    let num_elements = input_data.len();
+    let input_data_gpu = client.create(u32::as_bytes(&input_data));
+    unsafe {
+        kernel_plane_broadcast::launch::<R>(
+            &client,
+            CubeCount::Static(num_blocks as u32, 1, 1),
+            CubeDim::new(BLOCK_SIZE as u32, 1, 1),
+            ArrayArg::from_raw_parts::<u32>(&input_data_gpu, num_elements, 1),
+            ArrayArg::from_raw_parts::<u32>(&output_data_gpu, num_elements, 1),
+        )
+    }
+    let result = client.read_one(output_data_gpu.clone());
+    let output = u32::from_bytes(&result).to_vec();
+    println!("Plane Broadcast: {:?}", output);
 }
